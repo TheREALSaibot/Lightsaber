c8c91f2fffbb39317eb38457d1c91a3a103c026f
silvette <skrupaviciute@gmail.com>
2015-09-18 11:37:48 +0200

    WOW

--- app/src/main/java/com/example/blintbence/lightsaber/MainActivity.java
+++ app/src/main/java/com/example/blintbence/lightsaber/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
