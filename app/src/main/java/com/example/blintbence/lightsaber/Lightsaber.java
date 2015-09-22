a80c77ac63fc8682f96503c2d1f96aaaf4e40b18
BálintBence <vbalint9309@gmail.com>
2015-09-22 18:58:44 +0200

    light saber

--- app/src/main/java/com/example/blintbence/lightsaber/Lightsaber.java
+++ app/src/main/java/com/example/blintbence/lightsaber/Lightsaber.java
@@ -1,24 +1,67 @@
 package com.example.blintbence.lightsaber;
 
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class Lightsaber extends AppCompatActivity
 {
+    private MediaPlayer lsabersound;
+    private TextView lsaber;
+    private boolean isOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lightsaber);
+        isOn = false;
 
         TextView myTextView=(TextView)findViewById(R.id.title);
         Typeface typeFace= Typeface.createFromAsset(getAssets(), "fonts/starjedi.ttf");
         myTextView.setTypeface(typeFace);
+
+        lsabersound = MediaPlayer.create(this, R.raw.lf);
+        lsaber = (TextView) findViewById(R.id.lsaber);
+        lsaber.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (isOn)
+                {
+                    lsaber.setBackgroundResource(R.drawable.lsaberoff);
+                } else
+                {
+                    lsaber.setBackgroundResource(R.drawable.lsaberon);
+                }
+
+                lsabersound.start();
+            }
+        });
+
+    }
+
+    private void switchSaber()
+    {
+        if (isOn)
+        {
+            isOn = false;
+        } else
+        {
+            isOn = true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        lsabersound.release();
     }
 
     @Override
