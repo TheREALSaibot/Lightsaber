88be5c70666f7168509b447285a91859dead48e4
silvette <skrupaviciute@gmail.com>
2015-09-22 17:10:22 +0200

    updated with compass feature

--- app/src/main/java/com/example/blintbence/lightsaber/Compass.java
+++ app/src/main/java/com/example/blintbence/lightsaber/Compass.java
@@ -1,34 +1,59 @@
 package com.example.blintbence.lightsaber;
 
 import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Compass extends AppCompatActivity {
+public class Compass extends AppCompatActivity implements SensorEventListener{
 
     MediaPlayer march;
+    private ImageView compassImg;
+    private SensorManager sensormanager;
+    private Sensor accelerometer;
+    private Sensor magnometer;
+    private float currentCompassAngle = 0;
+    private float[] readingmagnometer = new float[3];
+    private float[] readingaccelerometer = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
-        TextView myTextView = (TextView) findViewById(R.id.title);
-        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Starjhol.ttf");
-        myTextView.setTypeface(typeFace);
-
         march = MediaPlayer.create(this, R.raw.imperialmarch);
         march.start();
+
+        compassImg = (ImageView) findViewById(R.id.compass);
+        sensormanager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensormanager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnometer = sensormanager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensormanager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensormanager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_GAME);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        sensormanager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensormanager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_GAME);
         march.release();
     }
 
@@ -52,5 +77,48 @@ public class Compass extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] rotation = new float[9];
+        float[] orientation = new float[3];
+
+        if(event.sensor == accelerometer)
+        {
+            readingaccelerometer[0] = event.values[0];
+            readingaccelerometer[1] = event.values[1];
+            readingaccelerometer[2] = event.values[2];
+        }
+        if(event.sensor == magnometer)
+        {
+            readingmagnometer[0] = event.values[0];
+            readingmagnometer[1] = event.values[1];
+            readingmagnometer[2] = event.values[2];
+        }
+
+        sensormanager.getRotationMatrix(rotation,null, readingaccelerometer, readingmagnometer);
+        sensormanager.getOrientation(rotation,orientation);
+        float azimuthRadians = orientation[0];
+        float azimuthDegrees = -(float)(Math.toDegrees(azimuthRadians)+360)%360;
+
+        doAnimation(currentCompassAngle, azimuthDegrees, compassImg);
+        currentCompassAngle = azimuthDegrees;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    private void doAnimation(float from, float to, View rotateme)
+    {
+        RotateAnimation ra = new RotateAnimation(from, to,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(200);
+        ra.setFillAfter(true);
+        rotateme.startAnimation(ra);
+
     }
 }
